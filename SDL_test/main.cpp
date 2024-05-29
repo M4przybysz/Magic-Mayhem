4467366c3cbfbf6ffc9b2462bb60b40672a415fd
Maciej <maprz2017@gmail.com>
2024-05-30 01:16:33 +0200

    added working SDL2 test

---
+++ SDL_test/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+const int WIDTH = 800, HEIGHT = 600;
+
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *Window = SDL_CreateWindow("test", 100, 100, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if(Window == NULL) {
+        std::cout << "Window not windowing!" << SDL_GetError() << "\n";
+        return 1;
+    }
+
+    SDL_Event WindowEvent;
+
+    while(true) {
+        if(SDL_PollEvent(&WindowEvent)) {
+            if(SDL_QUIT == WindowEvent.type) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(Window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
+}
