ed0b2323b92a353b2b2566597684245f4e5f75bd
ksoczowka <ksoczowka.ks@gmail.com>
2024-05-30 22:52:12 +0200

    added simple card class as example how to format code

---
+++ game/include/card.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+class Card {
+ public: // one space before classificator
+    Card();
+    Card(const int& cost); // const reference everything you can
+
+    int getCost() const { return cost_; } // one line funcs like getters and setters should be in class declaration like this // also const functions that doesn't change the object
+
+    void setCost(const int& cost) { cost_ = cost; }
+
+
+ private:
+    int cost_; // default values should be defined in constructors
+};
