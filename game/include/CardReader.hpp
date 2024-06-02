0d0b8e669b280e8d52b748a38936d763cb08fa9d
ksoczowka <ksoczowka.ks@gmail.com>
2024-06-02 16:12:08 +0200

    added getters and setters in CardInfo class

--- game/include/CardReader.hpp
+++ game/include/CardReader.hpp
@@ -12,6 +12,25 @@ class CardReader {
 
 class CardInfo {
  public:
+    CardInfo() {}
+
+    int getID() const { return id_; }
+    std::string getName() const { return name_; }
+    int getAttack() const { return attack_; }
+    int getHP() const { return hp_; }
+    int getCost() const { return cost_; }
+    int getPoints() const { return points_; }
+    std::string getDescription() const { return description_; }
+    std::string getImagePath() const { return imagePath_; }
+
+    void setID(const int& id) { id_ = id; }
+    void setName(const std::string& name) { name_ = name; }
+    void setAttack(const int& attack) { attack_ = attack; }
+    void setHP(const int& hp) { hp_ = hp; }
+    void setCost(const int& cost) { cost_ = cost; }
+    void setPoints(const int& points) { points_ = points; }
+    void setDescription(const std::string& description) { description_ = description; }
+    void setImagePath(const std::string& imagePath) { imagePath_ = imagePath; }
 
  private:
     int id_;
