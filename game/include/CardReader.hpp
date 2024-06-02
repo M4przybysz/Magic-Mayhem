d476e0beab3e8249cf796ec38beebe60527cb143
ksoczowka <ksoczowka.ks@gmail.com>
2024-06-02 15:54:45 +0200

    added 2 classes one for reader and second for card info

--- game/include/CardReader.hpp
+++ game/include/CardReader.hpp
@@ -1,2 +1,28 @@
 #pragma once
 
+#include <string>
+
+namespace Reader {
+class CardReader {
+ public:
+
+ private:
+    
+};
+
+class CardInfo {
+ public:
+
+ private:
+    int id_;
+    std::string name_;
+    int attack_;
+    int hp_;
+    int cost_;
+    int points_;
+    std::string description_;
+    std::string imagePath_;
+};
+}
+
+
