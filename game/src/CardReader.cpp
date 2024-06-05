5377f673bcd6287fc73c4fd1d85b59d8503ce0c4
ksoczowka <ksoczowka.ks@gmail.com>
2024-06-05 20:21:35 +0200

    added implementation of processFile function

--- game/src/CardReader.cpp
+++ game/src/CardReader.cpp
@@ -16,3 +16,31 @@ void Reader::CardReader::openFile(const std::string& path) {
 bool Reader::CardReader::isFileOpen() {
     return !file_.empty();
 }
+
+void Reader::CardReader::processFile() {
+    CardInfo info = CardInfo();
+    int id = 0;
+    for(const auto& el : file_) {
+        std::string nameOfAtribute = el.substr(0, el.find_first_of(':'));
+        
+        if (nameOfAtribute == "id") {
+            id = std::stoi(el.substr(el.find_first_of(':') + 1, 69));
+            info.setID(id);
+        } else if (nameOfAtribute == "name") {
+            info.setName(el.substr(el.find_first_of('\"') + 1, el.size() - el.find_first_of('\"') - 2));
+        } else if (nameOfAtribute == "attack") {
+            info.setAttack(std::stoi(el.substr(el.find_first_of(':') + 1, 69)));
+        } else if (nameOfAtribute == "hp") {
+            info.setHP(std::stoi(el.substr(el.find_first_of(':') + 1, 69)));
+        } else if (nameOfAtribute == "cost") {
+            info.setCost(std::stoi(el.substr(el.find_first_of(':') + 1, 69)));
+        } else if (nameOfAtribute == "points") {
+            info.setPoints(std::stoi(el.substr(el.find_first_of(':') + 1, 69)));
+        } else if (nameOfAtribute == "description") {
+            info.setDescription(el.substr(el.find_first_of('\"') + 1, el.size() - el.find_first_of('\"') - 2));
+        } else if (nameOfAtribute == "imagePath") {
+            info.setImagePath(el.substr(el.find_first_of('\"') + 1, el.size() - el.find_first_of('\"') - 2));
+            cards_.insert(std::pair<int, CardInfo>(id, info));
+        }
+    }
+}
