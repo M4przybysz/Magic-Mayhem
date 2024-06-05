0f113e0b9165b4027d0be5b5cb73ecbdf0564c60
ksoczowka <ksoczowka.ks@gmail.com>
2024-06-05 20:20:11 +0200

    main file with example of how to use reader

--- game/main.cpp
+++ game/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
 #include "include/CardReader.hpp"
+
+#include <iostream>
+#include <memory>
 
 
 int main(int argc, char *argv[]) {
+    std::shared_ptr<Reader::CardReader> reader = std::make_shared<Reader::CardReader>(Reader::CardReader());
+
+    reader->openFile("./cards.list");
+
+    if(!reader->isFileOpen()) {
+        std::cerr << "Error: File not opened!\n";
+        return 1;
+    }
+    else {
+        std::clog << "File opened successfully!\n";
+    }
+    reader->processFile();
     
 
     return 0;
